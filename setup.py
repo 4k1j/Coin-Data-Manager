52b4f19ba07809c704f609718c4b39cce2525963
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2021-07-31 12:48:58 +0900

    refactor: Set project structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='coin-bot',
+    version='1.0',
+    packages=['src', 'src.api', 'src.data', 'src.candle', 'src.candle.upbit', 'tests', 'tests.api'],
+    url='https://github.com/wjrmffldrhrl/coin-bot',
+    license='',
+    author='wjrmffldrhrl',
+    author_email='wjrmffldrhrl@gmail.com',
+    description='coin bot'
+)
